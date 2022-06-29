09496246c84e8b85715467bd3c5de2abdc8adf13
Shaun Millar <shaun.millar@nttdata.com>
2022-06-28 19:03:43 -0700

    First commit

--- digitalforms-api/src/main/java/ca/bc/gov/open/digitalformsapi/viirp/controller/SearchController.java
+++ digitalforms-api/src/main/java/ca/bc/gov/open/digitalformsapi/viirp/controller/SearchController.java
@@ -2,16 +2,18 @@ package ca.bc.gov.open.digitalformsapi.viirp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ca.bc.gov.open.digitalformsapi.viirp.api.SearchApi;
  
 /**
- * This controller for initial testing only. Please remove from project once permanent controllers are added.  
+ * VI IRP Search Controller   
  * 
  * @author 176899
  *
  */
 
 @RestController
-public class TestController {
+public class SearchController implements SearchApi{
  
     @RequestMapping("/test")
     public String hello() {

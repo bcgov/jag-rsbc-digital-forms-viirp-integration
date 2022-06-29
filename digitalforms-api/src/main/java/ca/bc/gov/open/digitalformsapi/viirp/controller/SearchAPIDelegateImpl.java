45012668b470d9d49355dff0cce74b906f2e0598
Shaun Millar <shaun.millar@nttdata.com>
2022-06-28 19:37:14 -0700

    final commit of working API (Search operations only)

---
+++ digitalforms-api/src/main/java/ca/bc/gov/open/digitalformsapi/viirp/controller/SearchAPIDelegateImpl.java
@@ -0,0 +1,21 @@
+package ca.bc.gov.open.digitalformsapi.viirp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import ca.bc.gov.open.digitalformsapi.viirp.api.SearchApiDelegate;
+import ca.bc.gov.open.digitalformsapi.viirp.model.SearchNoticeNumberServiceResponse;
+
+@Service
+public class SearchAPIDelegateImpl implements SearchApiDelegate {
+	
+	@Override
+	public ResponseEntity<SearchNoticeNumberServiceResponse> searchCorrelationIdGet(String correlationId,
+	        String noticeNo) {
+		
+		 System.out.println("Heard a call to search with correlationId " + correlationId + " and noticeNo : " + noticeNo);
+		
+		 return new ResponseEntity<>(HttpStatus.OK);
+	}
+}

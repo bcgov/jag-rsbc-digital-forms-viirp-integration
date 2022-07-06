9860fbbaf47974f0646344f563ce73ff489a075f
Burak Karahan <burak.karahan@nttdata.com>
2022-07-06 16:59:29 -0400

    Added swagger-ui config for displaying proper title, description and version info on the api ui

---
+++ digitalforms-api/src/main/java/ca/bc/gov/open/digitalformsapi/viirp/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package ca.bc.gov.open.digitalformsapi.viirp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+    public OpenAPI springShopOpenAPI() {
+	      return new OpenAPI().info(new Info()
+	              .title("VIPS Integration API")
+	              .description("VIPS Integration API")
+	              .version("v1.0"));
+	}
+
+}

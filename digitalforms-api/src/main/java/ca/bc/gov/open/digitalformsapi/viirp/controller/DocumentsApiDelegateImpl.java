5c3632022fd2e89f5d710972f4e4aead944f4aef
Shaun Millar <shaun.millar@nttdata.com>
2022-08-09 16:34:39 -0700

    DF-2070
    Completed body of store document operation.

--- digitalforms-api/src/main/java/ca/bc/gov/open/digitalformsapi/viirp/controller/DocumentsApiDelegateImpl.java
+++ digitalforms-api/src/main/java/ca/bc/gov/open/digitalformsapi/viirp/controller/DocumentsApiDelegateImpl.java
@@ -2,19 +2,28 @@ package ca.bc.gov.open.digitalformsapi.viirp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import ca.bc.gov.open.digitalformsapi.viirp.api.DocumentsApiDelegate;
+import ca.bc.gov.open.digitalformsapi.viirp.exception.DigitalFormsException;
 import ca.bc.gov.open.digitalformsapi.viirp.model.AssociateDocumentToNoticeServiceResponse;
 import ca.bc.gov.open.digitalformsapi.viirp.model.GetDocumentsListServiceResponse;
 import ca.bc.gov.open.digitalformsapi.viirp.model.StoreVIPSDocument;
 import ca.bc.gov.open.digitalformsapi.viirp.model.VipsDocumentResponse;
 import ca.bc.gov.open.digitalformsapi.viirp.model.VipsNoticeObj;
+import ca.bc.gov.open.digitalformsapi.viirp.utils.DigitalFormsConstants;
+import ca.bc.gov.open.jag.ordsvipsclient.api.DocumentApi;
+import ca.bc.gov.open.jag.ordsvipsclient.api.handler.ApiException;
+import ca.bc.gov.open.jag.ordsvipsclient.api.model.VipsDocumentOrdsResponse;
 
 @Service
 public class DocumentsApiDelegateImpl implements DocumentsApiDelegate{
+	
+	@Autowired
+	private DocumentApi documentApi;
 	
 	private final Logger logger = LoggerFactory.getLogger(DocumentsApiDelegateImpl.class);
 	
@@ -38,6 +47,15 @@ public class DocumentsApiDelegateImpl implements DocumentsApiDelegate{
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
+	/**
+	 * POST Document (Store document in VIPS) 
+	 * 
+	 * Possible Response codes:
+	 * 	201. created. 
+	 * 	401. unauth (digital forms basic auth failure)
+	 * 	500. from the SSG if VIPS WS is unavailable or responds with any code other than 200. 
+	 * @throws ApiException 
+	 */
 	@Override
 	public ResponseEntity<VipsDocumentResponse> documentsCorrelationIdPost(
 			String correlationId,
@@ -45,7 +63,44 @@ public class DocumentsApiDelegateImpl implements DocumentsApiDelegate{
 		
 		logger.info("Heard a call to the endpoint 'documentsCorrelationIdPost'");
 		
-		return new ResponseEntity<>(HttpStatus.OK);
+		VipsDocumentResponse resp = new VipsDocumentResponse();
+	
+		VipsDocumentOrdsResponse _resp;
+		try {
+			_resp = documentApi.storeDocumentPost(
+						storeVIPSDocument.getTypeCode(),			//required
+						storeVIPSDocument.getMimeType(), 			//required
+						storeVIPSDocument.getMimeSubType(),			//required	 
+						storeVIPSDocument.getAuthGuid(),			//required
+						storeVIPSDocument.getFileObject(), 			//required
+						storeVIPSDocument.getNoticeTypeCode(),		//optional 
+						storeVIPSDocument.getNoticeSubjectCode(),	//optional
+						storeVIPSDocument.getPageCount());			//optional
+			
+			// Depending on the result code from the VIPS store document call, set the response entity accordingly. 
+			if (_resp.getStatusCode().equals(String.valueOf(DigitalFormsConstants.VIPSWS_SUCCESS_CD))) {
+				resp.setDocumentId(_resp.getDocumentId());
+			} else if (_resp.getStatusCode().equals(String.valueOf(DigitalFormsConstants.VIPSWS_GENERAL_FAILURE_CD))) {
+				logger.error("VIPS Error: " + _resp.getStatusMessage());
+				throw new DigitalFormsException("Failed to store document document to VIPS WS. Type Code : " + storeVIPSDocument.getTypeCode() + 
+						". Mime sub type : " + storeVIPSDocument.getMimeSubType() + 
+						". Mime type : " + storeVIPSDocument.getMimeType());
+			} else if (_resp.getStatusCode().equals(String.valueOf(DigitalFormsConstants.VIPSWS_JAVA_EX))) {
+				logger.error("VIPS Error: " + _resp.getStatusMessage());
+				throw new DigitalFormsException("Internal Java error at VIPS WS. Failed to store document to VIPS WS. Type Code : " + storeVIPSDocument.getTypeCode() + 
+						". Mime sub type : " + storeVIPSDocument.getMimeSubType() +
+						". Mime type : " + storeVIPSDocument.getMimeType());
+			}
+			
+			return new ResponseEntity<>(resp, HttpStatus.OK);
+			
+			
+		} catch (ApiException e) {
+			logger.error(e.getMessage());
+			e.printStackTrace();
+			throw new DigitalFormsException(e.getMessage(), e);
+		}			 
+		
 	}
 	
 	@Override
